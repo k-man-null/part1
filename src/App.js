8fc80cc7c764712aee15d6afcf9ed346fe2ed3e7
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-12 00:21:01 +0300

    New App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from "react";
-const App = () => (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
+import React from "react"
 
-export default App;
+const App = () => {
+  const course = 'Half Stack Application Development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3= 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+    <p>
+      {part1} {exercises1}
+    </p>
+    <p>
+      {part2} {exercises2}
+    </p>
+    <p>
+      {part3} {exercises3}
+    </p>
+    <p>Number of exercises {exercises1+exercises2+exercises3}</p>      
+    </div>
+  ) 
+}
+
+export default App
+
+
