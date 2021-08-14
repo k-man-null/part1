5336cd8d18a068e059991ad7a7eab85ce701d1d6
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-14 17:06:57 +0300

    ''

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
-const Display = (props)=> {
+const Display = ({ counter })=> {
   return (
-    <div>{props.counter}</div>
+    <div>{counter}</div>
   )
 }
 
