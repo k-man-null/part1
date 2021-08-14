7cb5d127e2a4fd3e0f3931c296a45aa2d604b353
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-15 00:13:51 +0300

    Refactored Unicafe

--- src/App.js
+++ src/App.js
@@ -1,46 +1,65 @@
 import React, { useState } from "react"
+const StatisticLine = (props) => {
+  return (
+    <>
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
+    </>
+  )
+}
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
+const Statistics = (props) => {
+  console.log('Recieved props', props)
+  if (props.all ===0) {
     return (
-      <div>
-        the app is used by pressing buttons
-      </div>
+      <>
+      <h1 alt="Titele"> give feedback</h1>
+      <button onClick={props.handleGood}>good</button>
+      <button onClick={props.handleNeutral}>neutral</button>
+      <button onClick={props.handleBad}>bad</button>
+      <h1 alt="Title">you need to give feedback</h1>
+      </>
     )
   }
+  return (
+    <>
+    <h1> give feedback</h1>
+      <button onClick={props.handleGood}>good</button>
+      <button onClick={props.handleNeutral}>neutral</button>
+      <button onClick={props.handleBad}>bad</button>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={props.good}/>
+        <StatisticLine text="neutral" value={props.neutral}/>
+        <StatisticLine text="bad" value= {props.bad}/>
+        <StatisticLine text="all" value={props.all}/>
+        <StatisticLine text="average" value={props.average}/>
+        <StatisticLine text="positive" value={props.positive}/>
+        </tbody>
+      </table>
+    </>
+  )
+  
+}
 
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = ()=> setNeutral(neutral+ 1)
+  const handleBad = ()=> setBad(bad + 1)
+  const average = (good*1 + bad*-1)/(good+bad+neutral)
+  let all = good+neutral+bad
+  let positive = good/all*100 + '%'
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} handleGood={handleGood} handleNeutral={handleNeutral} handleBad={handleBad}/>
     </div>
   )
 }
-const App = () => { 
 
-const [left, setLeft] = useState(0)
-const [right, setRight] = useState(0)
-const [allClicks, setAll] = useState([])
-
-const handleLeftClick = () => {
-  setAll(allClicks.concat('L'))
-  setLeft(left + 1)
-}
-//'concat' is preferred to 'push' because it returns a copy of the array
-const handRightClick = () => {
-  setAll(allClicks.concat('R'))
-  setRight(right + 1)
-}
-
-return(
-  <div>
-    {left}
-    <button onClick={handleLeftClick}>Left</button>
-    <button onClick={handRightClick}>Right</button>
-    {right}
-    <History allClicks={allClicks}/>
-  </div>
-)
-}
 export default App
-
-
