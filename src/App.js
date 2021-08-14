4132dfa90f2217673159f151f4ac029605fdd3d3
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-14 15:46:13 +0300

    Component State with Intervals

--- src/App.js
+++ src/App.js
@@ -1,57 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
-function Header(props) {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
-  console.log('-------');
-  console.log(props);
-  console.log('-------');
-  return (
-    <>
-    <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-    <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-    <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-  </>
-  )
-}
-
-function Total(props){
-  return (
-    <p> Number of exercise {props.total}</p>
-  )
-}
 const App = () => {
-  const course = {
-    name: 'Half Stack Application Development',
-
-    parts: [
-    {
-    name:'Fundamentals of React',
-    exercises: 10
-    },
-
-    { 
-    name:'Using props to pass data',
-    exercises: 7
-    },
-
-    {
-    name: 'State of a component',
-    exercises: 14
+    let [ counter, setCounter ] = useState(10)
+    const finished = "!!Blast Off!!"
+    const end = setTimeout(
+      () => setCounter(counter - 1),
+      1000
+    )
+    if (counter == 0) {
+      clearInterval(end);
+      counter = undefined
     }
-  ]
-  }
-  return (
-    <div>
-      <Header course = {course.name} />
-      <Content parts = {course.parts}/>
-      <Total total = {course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>      
-    </div>
-  ) 
+      console.log('rendering ...', counter);
+    return (
+      <div>{counter??finished}</div>
+    )
 }
 
 export default App
