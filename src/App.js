3cdae219cc30e4128063a26115a34d37326989fd
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-14 16:32:24 +0300

    Pssing state to child components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react"
+const Display = (props)=> {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props)=> {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 const App = () => {
     const [ counter, setCounter ] = useState(0)
 
+    const increaseByOne = ()=> setCounter(counter + 1)
+    const decreaseByOne = ()=> setCounter(counter - 1)
+    const setToZero = ()=> setCounter(0)
     return (
       <div>
-        <div>{counter}</div>
-        <button onClick={()=> setCounter(counter + 1)}>
-          plus
-        </button>
-        <button onClick={()=> setCounter(counter - 1)}>
-          minus
-        </button>
-        <button onClick={()=> setCounter(0)}>
-          reset
-        </button>
+        <Display counter={counter}/>
+        <Button onClick={increaseByOne} text='plus'/>
+        <Button onClick={decreaseByOne} text='minus'/>
+        <Button onClick={setToZero} text='reset'/>
       </div>
     )
 }
