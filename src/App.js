43294a6ad690cd23b3fc97ac3d1438fc6fb81a95
Bill Konchellah <williamoriginal2019@gmail.com>
2021-08-14 19:41:57 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react"
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing buttons
+      </div>
+    )
+  }
 
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 const App = () => { 
 
 const [left, setLeft] = useState(0)
@@ -23,7 +37,7 @@ return(
     <button onClick={handleLeftClick}>Left</button>
     <button onClick={handRightClick}>Right</button>
     {right}
-    <p>{allClicks.join(' ')}</p>
+    <History allClicks={allClicks}/>
   </div>
 )
 }
